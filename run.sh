f51ac6147d8d65009182de7afbc93400c387298b
Steve McQuaid <steve@stevemcquaid.com>
2017-04-26 14:04:25 -0700

    Add stevemcquaid/grip:latest tags

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker build . -t grip-docker &> /dev/null
-docker stop grip-docker &> /dev/null
-docker rm grip-docker &> /dev/null
-CONTAINER=$(docker run -d -v "$(pwd)":/src --name grip-docker -P grip-docker:latest grip $1 0.0.0.0:8080) &> /dev/null
-CONTAINERPORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' grip-docker) &> /dev/null
+docker build . -t stevemcquaid/grip:latest
+docker stop grip
+docker rm grip
+CONTAINERID=$(docker run -d -v "$(pwd)":/src --name grip -P stevemcquaid/grip:latest grip $1 0.0.0.0:8080)
+CONTAINERPORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' grip)
 echo "..."
-sleep 2 &> /dev/null
-open http://localhost:$CONTAINERPORT &> /dev/null
+sleep 2
+open http://localhost:$CONTAINERPORT
